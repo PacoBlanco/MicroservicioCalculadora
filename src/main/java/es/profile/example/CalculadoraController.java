70cf3c5d067de004a43a3ba487784074bd6e6bd1
FranciscoJavierBlanco <fjblanco@profile.es>
2020-04-08 18:26:40 +0200

    Commit inicial del proyecto: arranca SpringBoot sobre Tomcat y se hace dummy de la CalculadoraController

---
+++ src/main/java/es/profile/example/CalculadoraController.java
@@ -0,0 +1,10 @@
+package es.profile.example;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculadoraController {
+	
+	
+
+}
---
+++ src/main/java/es/profile/example/MicroservicioCalculadoraApplication.java
@@ -0,0 +1,13 @@
+package es.profile.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCalculadoraApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(MicroservicioCalculadoraApplication.class, args);
+	}
+	
+}
