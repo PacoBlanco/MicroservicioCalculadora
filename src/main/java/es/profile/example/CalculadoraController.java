ad10d297d189af9eb6e081c54e276be00f8e0313
FranciscoJavierBlanco <fjblanco@profile.es>
2020-04-08 19:30:58 +0200

    Se mete la validación de entrada en la operación sobre el recurso (aunque en el dto ya se había metido las anotaciones pertinentes). Se añade, aunque comentado, el manejo de errores que podía tener el microservicio para devolver una excepcion/objeto más normalizada para añadir en la especificación del microservicio

--- src/main/java/es/profile/example/CalculadoraController.java
+++ src/main/java/es/profile/example/CalculadoraController.java
@@ -2,6 +2,8 @@ package es.profile.example;
 
 import java.math.BigDecimal;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +20,22 @@ public class CalculadoraController {
 	
 	@RequestMapping(value = "operaciones", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
-	public BigDecimal operar(@RequestBody Operacion operacion) {
+	public BigDecimal operar(@Valid @RequestBody Operacion operacion) {
 		return operacion.computar();
 	}
+	
+//	/**
+//	 * Gestión de errores en el caso de querer retornar una excepción/un objeto de un tipo según la especificación a definir de salida del Microservicio que se quiera
+//	 * 
+//	 * @param e
+//	 * @param request
+//	 * @param response
+//	 * @return
+//	 */
+//	@ExceptionHandler
+//	public @ResponseBody
+//	Object handleException(Exception e, HttpServletRequest request, HttpServletResponse response) {
+//		return e;
+//	}
 
 }
